c2498bab3a49c306ca31bfaf7f315e39bf66a180
Alena <kudryashova_a_n@example.com>
2022-08-03 23:02:39 +0300

    task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray (String[] col){
+    for(int i=0; i<col.Length; i++){
+        Console.Write(col[i] +" ");
+    }
+    Console.WriteLine();
+}
+String [] array = {"Hi", "1234", "Hi!"};
+String [] result = new String[array.Length];
+
+for (int i=0; i<array.Length; i++){
+    if(array[i].Length <=3){
+        result[i]=array[i];
+    }
+}
+
+PrintArray (result);
+
+
